7741ef76248deb1462653d350c8f963cfaf3ccf1
Nontonyuk <kinikita.xyz@gmail.com>
2026-04-24 21:50:33 +0700

    Update API endpoint to use gemini-2.5-flash

--- api/generate.js
+++ api/generate.js
@@ -144,8 +144,8 @@ WAJIB:
 `;
 
     const endpoint =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
-      GEMINI_API_KEY;
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" +
+  GEMINI_API_KEY;
 
     const geminiResponse = await fetch(endpoint, {
       method: "POST",
