6bc6e3aa02aa3be1542d4c90420a9f8d1b52efc3
Nontonyuk <kinikita.xyz@gmail.com>
2026-04-24 21:27:16 +0700

    Add success response to generate handler

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,8 @@
 module.exports = async function handler(req, res) {
+  res.status(200).json({
+    success: true
+  });
+}
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
